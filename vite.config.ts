1d3e480ccc6b06ec160f4bf711c5e836cb9f79d3
mousata-med <moustamuhammed@gmail.com>
2025-05-24 11:25:27 +0100

    changing project port & remove unused files

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+  },
   plugins: [react(), tailwindcss()],
 });
