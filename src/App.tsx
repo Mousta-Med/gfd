4d7f330888f70a7dcdce4278184ca59a7abc1360
mousata-med <moustamuhammed@gmail.com>
2025-05-26 15:57:54 +0100

    add Navbar and Footer components with navigation structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <div
+        className="min-h-screen text-white flex items-center justify-center"
+        style={{ backgroundColor: "rgba(9, 9, 15, 1)" }}
+      ></div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
